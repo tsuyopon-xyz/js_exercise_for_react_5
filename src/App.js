a377930dfa1adb681674b754fe200bbcf0dbfdbf
tsuyopon-xyz <tsuyoshi4business+tsuyopon.xyz@gmail.com>
2019-08-15 13:17:35 +0900

    Created an example answer code.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <h1>課題5</h1>;
+const API_URL = 'https://opentdb.com/api.php?amount=10';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    this.requestData();
+  }
+
+  async requestData() {
+    this.setState({ loading: true });
+
+    let quizList;
+    try {
+      const response = await window.fetch(API_URL);
+      const data = await response.json();
+      quizList = data.results;
+    } catch (error) {
+      quizList = null;
+    }
+
+    this.setState({
+      data: quizList,
+      loading: false
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        { this.renderRequestButton() }
+        { this.renderData() }
+      </div>
+    );
+  }
+
+  renderData() {
+    if (this.state.loading) {
+      return <p>データ取得中...</p>;
+    }
+    if (!this.state.loading && !this.state.data) {
+      return <p>データなし</p>;
+    }
+
+    const quizItems = this.state.data.map( (quiz, index) => {
+      const quizNumber = index + 1;
+      return (
+        <li key={quizNumber}>Q{ quizNumber } : {quiz.question}</li>
+      );
+    });
+
+    return <ul>{ quizItems }</ul>;
+  }
+
+  renderRequestButton() {
+    if (this.state.loading) {
+      return <button disabled>データ取得中...</button>;
+    }
+
+    return <button onClick={ () => { this.requestData() } }>データを取得する</button>;
+  }
 }
 
 export default App;
